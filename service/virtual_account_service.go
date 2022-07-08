f106a1cedcd2a2aa3856aad5cc023008b6f6c47b
Arif Widiasan Subagio <arifw.subagio17@gmail.com>
2022-07-08 22:43:43 +0700

    remove update VA & add create fixed VA Xendit

--- service/virtual_account_service.go
+++ service/virtual_account_service.go
@@ -3,38 +3,48 @@ package service
 import (
 	"fmt"
 
+	"github.com/CapstoneProject31/backend_ppob_31/config"
 	"github.com/CapstoneProject31/backend_ppob_31/model"
-	nanoid "github.com/aidarkhanov/nanoid/v2"
+	"github.com/xendit/xendit-go"
+	"github.com/xendit/xendit-go/virtualaccount"
 )
+
+func newTrue() *bool {
+	b := true
+	return &b
+}
 
 func (s *svc) CreateVirtualAccountService(transaction model.Transaction) error {
 	virtual_account := model.Virtual_account{}
 
-	id, _ := nanoid.New()
+	VA_Number := transaction.Payment_Method.Range + uint64(transaction.ID)
+	String_VA_Number := fmt.Sprintf("%d", VA_Number)
 
-	virtual_account.VAID = id
-	virtual_account.ExternalID = transaction.CodeTransaction
+	xendit.Opt.SecretKey = config.InitConfiguration().XENDITH_API
+	data := virtualaccount.CreateFixedVAParams{
+		ExternalID:           transaction.CodeTransaction,
+		BankCode:             transaction.Payment_Method.CodeBank,
+		Name:                 transaction.User.Name,
+		VirtualAccountNumber: String_VA_Number,
+		IsClosed:             newTrue(),
+		ExpectedAmount:       float64(transaction.Price),
+	}
 
-	number := transaction.Payment_Method.Range + uint64(transaction.User.ID)
-	virtual_account.VANumber = fmt.Sprintf("%s%d", transaction.Payment_Method.CodeVA, number)
+	resp, err2 := virtualaccount.CreateFixedVA(&data)
+	if err2 != nil {
+		return fmt.Errorf("error create VA on Xendith")
+	}
 
-	virtual_account.Price = transaction.Price
+	virtual_account.VANumber = fmt.Sprintf("%s%d", transaction.Payment_Method.CodeVA, VA_Number)
+	virtual_account.VAID = resp.ID
+	virtual_account.ExternalID = resp.ExternalID
+	virtual_account.Price = uint(resp.ExpectedAmount)
 	virtual_account.UserID = transaction.UserID
 	virtual_account.PaymentMethodID = transaction.PaymentMethodID
 
-	res, err := s.repo.CheckVirtualAccount(int(virtual_account.UserID), int(virtual_account.PaymentMethodID))
+	err := s.repo.CreateVirtualAccount(virtual_account)
 	if err != nil {
-		err = s.repo.CreateVirtualAccount(virtual_account)
-		if err != nil {
-			return err
-		}
-	} else {
-		res.ExternalID = virtual_account.ExternalID
-		res.Price = virtual_account.Price
-		err = s.repo.UpdateVirtualAccountByID(int(res.ID), res)
-		if err != nil {
-			return err
-		}
+		return err
 	}
 
 	return nil
